7539cdb9178a35372a268c0cdec34b46e6aea5ed
adityranjan1011 <adityamnnit009@gmail.com>
2020-08-20 16:17:16 +0530

    Updates

---
+++ precache-manifest.0612932b735ccfde2a88f930dce31c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce7a58b1b2ac6e8c233b6c1f9ad130e",
+    "url": "/collegeDuniya/index.html"
+  },
+  {
+    "revision": "3b65067762d2441d1d5f",
+    "url": "/collegeDuniya/static/css/main.f7225747.chunk.css"
+  },
+  {
+    "revision": "869437daaa61186d487c",
+    "url": "/collegeDuniya/static/js/2.40308c22.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/collegeDuniya/static/js/2.40308c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b65067762d2441d1d5f",
+    "url": "/collegeDuniya/static/js/main.6850c215.chunk.js"
+  },
+  {
+    "revision": "55d725669751d93dbe3e",
+    "url": "/collegeDuniya/static/js/runtime-main.bfa39a32.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/collegeDuniya/static/media/college_02.619ee97e.jpg"
+  }
+]);
